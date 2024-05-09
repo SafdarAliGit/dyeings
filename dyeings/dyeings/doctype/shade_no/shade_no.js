34ef705bf7afb6cbbedeb7a24091b9d9a234c43e
safdar211ali <safdar211@hotmail.com>
2024-05-09 21:42:13 +0500

    updated

---
+++ dyeings/dyeings/doctype/shade_no/shade_no.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Techventures and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shade No', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ dyeings/dyeings/doctype/shade_no/test_shade_no.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Techventures and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestShadeNo(FrappeTestCase):
+	pass
