6c8d120d17d21bce770dc683c8abb6cea98bb527
safdar211ali <safdar211@hotmail.com>
2024-05-10 13:10:42 +0500

    updated

---
+++ dyeings/dyeings/doctype/over_head_account/over_head_account.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Techventures and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Over Head Account', {
+	// refresh: function(frm) {
+
+	// }
+});
