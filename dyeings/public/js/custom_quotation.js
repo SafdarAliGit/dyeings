d824c5c9b46b3d7a3b47eccfa00a4bb3d50c275b
safdar211ali <safdar211@hotmail.com>
2024-05-12 21:54:09 +0500

    updated

--- dyeings/public/js/custom_quotation.js
+++ dyeings/public/js/custom_quotation.js
@@ -1,15 +1,14 @@
 frappe.ui.form.on("Quotation", {
     refresh: function (frm) {
-        frm.add_custom_button(__('From Shade Process'), () => {
-            // Define dialog variable
-            let dialog;
-
+        frm.add_custom_button(__('From Shade Process'), function () {
             // MultiSelectDialog for individual child selection
             new frappe.ui.form.MultiSelectDialog({
                 doctype: "Shade Process",
                 target: frm,
                 setters: {
-                    total_cost: null, customer: frm.doc.party_name,fabric_type:null
+                    total_cost: null,
+                    customer: frm.doc.party_name,
+                    fabric_type: null
                 },
                 add_filters_group: 1,
                 date_field: "transaction_date",
@@ -17,51 +16,47 @@ frappe.ui.form.on("Quotation", {
                 get_query() {
                     return {
                         filters: {docstatus: ['=', 1]}
-                    }
-                }, action(selections) {
-                    frm.clear_table('items');
+                    };
+                },
+                action(selections) {
                     if (selections) {
-
                         frappe.call({
                             method: 'dyeings.dyeings.utils.from_shade_process.from_shade_process',
-                            args: {
-                                names: selections // Sending list of names
-                            }, callback: function (r) {
-                                // Clear existing table
-                                // frm.clear_table('items');
-                                if (r.message) {
-                                    // Add items to table
+                            args: {names: selections}, // Sending list of names
+                            callback: function (r) {
+                                if (r.message && r.message.shade_process) {
+                                    let items = frm.doc.items || [];
                                     r.message.shade_process.forEach(function (i) {
-                                        let entry = frm.add_child("items");
-                                        entry.shade_process = i.name,
-                                            entry.cost_rate = i.total_cost,
-                                            entry.qty = 1,
-                                            entry.fabric_type = i.fabric_type
-                                    })
-                                    // Refresh field
+                                        let existing_item = items.find(item => item.shade_process === i.name);
+                                        if (existing_item) {
+                                            existing_item.qty += 1;
+                                        } else {
+                                            let entry = frm.add_child("items");
+                                            entry.shade_process = i.name;
+                                            entry.cost_rate = i.total_cost;
+                                            entry.qty = 1;
+                                            entry.fabric_type = i.fabric_type;
+                                        }
+                                    });
                                     frm.refresh_field('items');
                                 }
-                                // Hide the dialog after action
-
                             }
                         });
                     }
-                    // Hide the dialog after action
-                  this.dialog.hide();
+                    this.dialog.hide();
                 }
             }).show();
         }, __('Get Items From'));
     }
 });
 
-
 frappe.ui.form.on('Quotation Item', {
     profit_percentage: function (frm, cdt, cdn) {
-        calcualte_rate(frm, cdt, cdn);
+        calculate_rate(frm, cdt, cdn);
     }
 });
 
-function calcualte_rate(frm, cdt, cdn) {
+function calculate_rate(frm, cdt, cdn) {
     var row = locals[cdt][cdn];
     frappe.model.set_value(cdt, cdn, 'rate', (flt(row.cost_rate) * (flt(row.profit_percentage) / 100)) + flt(row.cost_rate));
 }
