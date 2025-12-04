1295c1af5b8c937111414de4cf4e10099a60be53
SafdarAliGit <safdar211@gmail.com>
2025-12-04 22:24:48 +0500

    udpated

--- dyeings/public/js/delivery_note.js
+++ dyeings/public/js/delivery_note.js
@@ -1,33 +1,38 @@
 frappe.ui.form.on("Delivery Note", {
-     refresh: function (frm) {
-        // Run on form load and refresh
-     
+    refresh(frm) {
+        // Runs on load and refresh
     }
 });
 
 frappe.ui.form.on("Delivery Note Item", {
-     qty: function (frm, cdt, cdn) {
-        alert("qty changed");
-        console.log("qty changed", frm, cdt, cdn);
+    qty(frm, cdt, cdn) {
+        console.log("qty changed");
         waste_qty_and_waste_percent(frm, cdt, cdn);
     },
-    batch_no: function (frm, cdt, cdn) {
-        alert("batch_no changed");
-        console.log("batch_no changed", frm, cdt, cdn);
+    batch_no(frm, cdt, cdn) {
+        console.log("batch_no changed");
         waste_qty_and_waste_percent(frm, cdt, cdn);
     }
 });
 
 
-
 function waste_qty_and_waste_percent(frm, cdt, cdn) {
-    var row = locals[cdt][cdn];
-    var waste_qty = (row.custom_qty_received || 0) - (row.qty || 0);
-    frappe.model.set_value(cdt, cdn, 'custom_waste_qty', waste_qty);
-    
-    // Also update waste percentage if needed
-    if (row.custom_qty_received > 0) {
-        var waste_percent = (waste_qty / (row.custom_qty_received || 1)) * 100;
-        frappe.model.set_value(cdt, cdn, 'custom_waste_percentage', waste_percent);
+    let row = locals[cdt][cdn];
+
+    // Convert safely to numbers
+    let received_qty = Number(row.custom_qty_received) || 0;
+    let qty = Number(row.qty) || 0;
+
+    // Calculate waste qty
+    let waste_qty = received_qty - qty;
+
+    frappe.model.set_value(cdt, cdn, "custom_waste_qty", waste_qty);
+
+    // Calculate waste percentage
+    if (received_qty > 0) {
+        let waste_percent = (waste_qty / received_qty) * 100;
+        frappe.model.set_value(cdt, cdn, "custom_waste_percentage", waste_percent);
+    } else {
+        frappe.model.set_value(cdt, cdn, "custom_waste_percentage", 0);
     }
 }
