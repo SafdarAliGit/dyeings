fc51e2aea03c650209e0104ed0e936ea297166cc
SafdarAliGit <safdar211@gmail.com>
2025-12-04 21:55:19 +0500

    udpated

---
+++ dyeings/public/js/delivery_note.js
@@ -0,0 +1,22 @@
+frappe.ui.form.on("Delivery Note Item", {
+     qty: function (frm, cdt, cdn) {
+        waste_qty_and_waste_percent(frm, cdt, cdn);
+    },
+    batch_no: function (frm, cdt, cdn) {
+        waste_qty_and_waste_percent(frm, cdt, cdn);
+    }
+});
+
+
+
+function waste_qty_and_waste_percent(frm, cdt, cdn) {
+    var row = locals[cdt][cdn];
+    var waste_qty = (row.custom_qty_received | 0) - (row.qty | 0);
+    frappe.model.set_value(cdt, cdn, 'custom_waste_qty', waste_qty);
+    
+    // Also update waste percentage if needed
+    if (row.custom_qty_received > 0) {
+        var waste_percent = (waste_qty / row.custom_qty_received) * 100;
+        frappe.model.set_value(cdt, cdn, 'custom_waste_percentage', waste_percent);
+    }
+}
